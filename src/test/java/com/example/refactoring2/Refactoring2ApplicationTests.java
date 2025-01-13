11d69cdb0b95b9ab297f574671553bb402a78eb3
이진우 <leejinwoo1126@gmail.com>
2025-01-13 17:05:04 +0900

    init

---
+++ src/test/java/com/example/refactoring2/Refactoring2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.refactoring2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Refactoring2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
