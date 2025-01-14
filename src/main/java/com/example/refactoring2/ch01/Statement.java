3cf508d9b6b250549dff8b95ae72321766a5483c
이진우 <leejinwoo1126@gmail.com>
2025-01-14 13:53:31 +0900

    feat: htmlStatement(), renderHtml() 메서드 구현

--- src/main/java/com/example/refactoring2/ch01/Statement.java
+++ src/main/java/com/example/refactoring2/ch01/Statement.java
@@ -18,4 +18,25 @@ public class Statement {
         result.append(String.format("적립 포인트: %d점\n", data.getTotalVolumeCredits()));
         return result.toString();
     }
+
+    public String htmlStatement(Invoice invoice, Plays plays) throws Exception {
+        return renderHtml(StatementData.createStatementData(invoice, plays));
+    }
+
+    private String renderHtml(StatementData data) {
+        StringBuilder result = new StringBuilder();
+        result.append(String.format("<h1>청구 내역 (고객명: %s)</h1>\n", data.getCustomer()));
+
+        result.append("<table>\n");
+        result.append("<tr><th>연극</th><th>좌석 수</th><th>금액</th></tr>\n");
+        for(EnrichPerformance performance : data.getEnrichPerformances()) {
+            result.append(String.format("<tr><td>%s</td><td>%d석</td>", performance.getPlayName(), performance.getAudience()));
+            result.append(String.format("<td>$%d</td></tr>\n", performance.getAmount() / 100));
+        }
+        result.append("</table>\n");
+
+        result.append(String.format("<p>총액: <em>$%d</em></p>\n", data.getTotalAmount() / 100));
+        result.append(String.format("<p>적립 포인트: <em>%d점</em></p>\n", data.getTotalVolumeCredits()));
+        return result.toString();
+    }
 }
