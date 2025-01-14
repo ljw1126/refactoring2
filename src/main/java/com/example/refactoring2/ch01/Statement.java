d659f0129b4eda44ac1e1c89cd84c26a3222d010
이진우 <leejinwoo1126@gmail.com>
2025-01-14 12:34:32 +0900

    refactor: Statement 클래스내 불필요한 메서드 제거

--- src/main/java/com/example/refactoring2/ch01/Statement.java
+++ src/main/java/com/example/refactoring2/ch01/Statement.java
@@ -23,16 +23,6 @@ public class Statement {
         return renderPlainText(data);
     }
 
-    private EnrichPerformance create(Performance performance, Plays plays) throws Exception {
-        String playId = performance.getPlayId();
-        int audience = performance.getAudience();
-        Play play = plays.get(playId);
-        int amount = amountFor(performance, play);
-        int volumeCredits = volumeCreditsFor(performance, play);
-
-        return new EnrichPerformance(playId, audience, play, amount, volumeCredits);
-    }
-
     private String renderPlainText(StatementData data) throws Exception {
         StringBuilder result = new StringBuilder();
         result.append(String.format("청구 내역 (고객명: %s)", data.getCustomer())).append("\n");
@@ -44,40 +34,6 @@ public class Statement {
         result.append(String.format("총액: $%d\n", totalAmount(data) / 100));
         result.append(String.format("적립 포인트: %d점\n", totalVolumeCredits(data)));
         return result.toString();
-    }
-
-    private int volumeCreditsFor(Performance performance, Play play) {
-        int result = 0;
-        result += Math.max(performance.getAudience() - 30, 0);
-
-        if(play.getType().equals(PlayType.COMEDY)) {
-            result += (performance.getAudience() / 5);
-        }
-
-        return result;
-    }
-
-    private int amountFor(Performance performance, Play play) throws Exception {
-        int result;
-        switch (play.getType()) {
-            case TRAGEDY :
-                result = 40_000;
-                if(performance.getAudience() > 30) {
-                    result += 1_000 * (performance.getAudience() - 30);
-                }
-                break;
-            case COMEDY :
-                result = 30_000;
-                if(performance.getAudience() > 30) {
-                    result += 10_000 + 500 * (performance.getAudience() - 20);
-                }
-                result += 300 * performance.getAudience();
-                break;
-            default :
-                throw new Exception(String.format("알 수 없는 장르: %s", play.getType()));
-        }
-
-        return result;
     }
 
     private int totalAmount(StatementData data) throws Exception {
