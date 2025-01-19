474f73a5040a0f2331adece9396ab3c3689a5979
이진우 <leejinwoo1126@gmail.com>
2025-01-19 23:52:42 +0900

    refactor: Statement usd() 생성, 함수 인라인 적용

--- src/main/java/com/example/refactoring2/ch01/Statement.java
+++ src/main/java/com/example/refactoring2/ch01/Statement.java
@@ -1,5 +1,8 @@
 package com.example.refactoring2.ch01;
 
+import java.text.NumberFormat;
+import java.util.Locale;
+
 public class Statement {
 
     public String statement(Invoice invoice, Plays plays) {
@@ -11,10 +14,10 @@ public class Statement {
         result.append(String.format("청구 내역 (고객명: %s)", data.getCustomer())).append("\n");
 
         for(EnrichPerformance performances : data.getEnrichPerformances()) {
-            result.append(String.format("%s: $%d %d석\n", performances.getPlayName(), performances.getAmount() / 100, performances.getAudience()));
+            result.append(String.format("%s: %s %d석\n", performances.getPlayName(), usd(performances.getAmount()), performances.getAudience()));
         }
 
-        result.append(String.format("총액: $%d\n", data.getTotalAmount() / 100));
+        result.append(String.format("총액: %s\n", usd(data.getTotalAmount())));
         result.append(String.format("적립 포인트: %d점\n", data.getTotalVolumeCredits()));
         return result.toString();
     }
@@ -31,12 +34,17 @@ public class Statement {
         result.append("<tr><th>연극</th><th>좌석 수</th><th>금액</th></tr>\n");
         for(EnrichPerformance performance : data.getEnrichPerformances()) {
             result.append(String.format("<tr><td>%s</td><td>%d석</td>", performance.getPlayName(), performance.getAudience()));
-            result.append(String.format("<td>$%d</td></tr>\n", performance.getAmount() / 100));
+            result.append(String.format("<td>%s</td></tr>\n", usd(performance.getAmount())));
         }
         result.append("</table>\n");
 
-        result.append(String.format("<p>총액: <em>$%d</em></p>\n", data.getTotalAmount() / 100));
+        result.append(String.format("<p>총액: <em>%s</em></p>\n",  usd(data.getTotalAmount())));
         result.append(String.format("<p>적립 포인트: <em>%d점</em></p>\n", data.getTotalVolumeCredits()));
         return result.toString();
+    }
+
+    private String usd(int aNumber) {
+        NumberFormat format = NumberFormat.getCurrencyInstance(Locale.US);
+        return format.format(aNumber / 100);
     }
 }
--- src/test/java/com/example/refactoring2/ch01/StatementTest.java
+++ src/test/java/com/example/refactoring2/ch01/StatementTest.java
@@ -27,10 +27,10 @@ class StatementTest {
         Plays plays = new Plays(playMap);
 
         String expected = "청구 내역 (고객명: BigCo)\n" +
-                "hamlet: $650 55석\n" +
-                "As You Like It: $580 35석\n" +
-                "Othello: $500 40석\n" +
-                "총액: $1730\n" +
+                "hamlet: $650.00 55석\n" +
+                "As You Like It: $580.00 35석\n" +
+                "Othello: $500.00 40석\n" +
+                "총액: $1,730.00\n" +
                 "적립 포인트: 47점\n";
 
         Statement statement = new Statement();
@@ -56,11 +56,11 @@ class StatementTest {
         String expected = "<h1>청구 내역 (고객명: BigCo)</h1>\n" +
                 "<table>\n" +
                 "<tr><th>연극</th><th>좌석 수</th><th>금액</th></tr>\n" +
-                "<tr><td>hamlet</td><td>55석</td><td>$650</td></tr>\n" +
-                "<tr><td>As You Like It</td><td>35석</td><td>$580</td></tr>\n" +
-                "<tr><td>Othello</td><td>40석</td><td>$500</td></tr>\n" +
+                "<tr><td>hamlet</td><td>55석</td><td>$650.00</td></tr>\n" +
+                "<tr><td>As You Like It</td><td>35석</td><td>$580.00</td></tr>\n" +
+                "<tr><td>Othello</td><td>40석</td><td>$500.00</td></tr>\n" +
                 "</table>\n" +
-                "<p>총액: <em>$1730</em></p>\n" +
+                "<p>총액: <em>$1,730.00</em></p>\n" +
                 "<p>적립 포인트: <em>47점</em></p>\n";
 
         Statement statement = new Statement();
