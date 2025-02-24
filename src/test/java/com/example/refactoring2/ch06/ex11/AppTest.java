65c32b4a289390eac080b7cf73039bf650709253
이진우 <leejinwoo1126@gmail.com>
2025-02-24 18:50:10 +0900

    refactor: 6.11 parseCommandLine() 메서드 추출

--- src/test/java/com/example/refactoring2/ch06/ex11/AppTest.java
+++ src/test/java/com/example/refactoring2/ch06/ex11/AppTest.java
@@ -1,24 +1,16 @@
 package com.example.refactoring2.ch06.ex11;
 
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import java.io.PrintStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class AppTest {
-
-    private final PrintStream originalOut = System.out;
-    private final PrintStream originalErr = System.err;
-    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
-    private final ByteArrayOutputStream errContent = new ByteArrayOutputStream();
 
     private final String sampleJsonData = """
     [
@@ -28,37 +20,15 @@ class AppTest {
     ]
     """;
 
-    @BeforeEach
-    void setUp() {
-        System.setOut(new PrintStream(outContent));
-        System.setErr(new PrintStream(errContent));
-    }
-
     @DisplayName("빈 문자열 배열을 입력하면 예외를 출력한다")
     @Test
     void emptyStringArray() {
-        App.main(new String[] {});
-        assertThat(errContent.toString().trim()).isEqualTo("파일명을 입력하세요");
+        assertThatThrownBy(() -> App.run(new String[] {}))
+                .isInstanceOf(RuntimeException.class)
+                .hasMessage("파일명을 입력하세요");
     }
 
     @DisplayName("-r 옵션이 있으면, ready 상태의 주문 수를 출력한다")
-    @Test
-    void printOrderCountWithOption() throws IOException {
-        Path tempFile = createTempJsonFile(sampleJsonData);
-        App.main(new String[] {"-r", tempFile.toString()});
-
-        assertThat(outContent.toString().trim()).isEqualTo("2");
-    }
-
-    @DisplayName("-r 옵션이 없으면 Order 배열의 길이를 출력한다")
-    @Test
-    void printOrderCountWithoutOption() throws IOException {
-        Path tempFile = createTempJsonFile(sampleJsonData);
-        App.main(new String[] {tempFile.toString()});
-
-        assertThat(outContent.toString().trim()).isEqualTo("3");
-    }
-
     @Test
     void runWithOption() throws IOException {
         Path tempFile = createTempJsonFile(sampleJsonData);
@@ -68,6 +38,7 @@ class AppTest {
         assertThat(actual).isEqualTo(2);
     }
 
+    @DisplayName("-r 옵션이 없으면 Order 배열의 길이를 출력한다")
     @Test
     void runWithoutOption() throws IOException {
         Path tempFile = createTempJsonFile(sampleJsonData);
@@ -84,9 +55,4 @@ class AppTest {
         return tempFile;
     }
 
-    @AfterEach
-    void tearDown() {
-        System.setOut(originalOut);
-        System.setErr(originalErr);
-    }
 }
