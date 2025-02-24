7e3a06837d78929b002dc3e563d448cff7e7c788
이진우 <leejinwoo1126@gmail.com>
2025-02-24 18:47:18 +0900

    refactor: 6.11 CommandLine 으로 중간 데이터 이동

--- src/main/java/com/example/refactoring2/ch06/ex11/App.java
+++ src/main/java/com/example/refactoring2/ch06/ex11/App.java
@@ -19,15 +19,17 @@ public class App {
     static long run(String[] args) throws IOException {
         if(args.length == 0) throw new RuntimeException("파일명을 입력하세요");
         CommandLine commandLine = new CommandLine();
-        String fileName = args[args.length - 1];
-        return countOrders(commandLine, args, fileName);
+        commandLine.fileName = args[args.length - 1];
+        commandLine.onlyCountReady = Stream.of(args).anyMatch(arg -> "-r".equals(arg));
+        return countOrders(commandLine);
     }
 
-    private static long countOrders(CommandLine commandLine, String[] args, String fileName) throws IOException {
-        File input = Paths.get(fileName).toFile();
+    private static long countOrders(CommandLine commandLine) throws IOException {
+        File input = Paths.get(commandLine.fileName).toFile();
         ObjectMapper mapper = new ObjectMapper();
         Order[] orders = mapper.readValue(input, Order[].class);
-        if(Stream.of(args).anyMatch(arg -> "-r".equals(arg))) {
+
+        if(commandLine.onlyCountReady) {
             return Stream.of(orders)
                     .filter(o -> "ready".equals(o.status()))
                     .count();
@@ -37,6 +39,9 @@ public class App {
     }
 
     private static class CommandLine {
+        public String fileName;
+        public boolean onlyCountReady;
+
         public CommandLine() {
         }
     }
