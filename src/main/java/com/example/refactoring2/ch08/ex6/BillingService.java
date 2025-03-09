ffe95c2d759422309c0601011436717b1e23216d
이진우 <leejinwoo1126@gmail.com>
2025-03-09 16:32:32 +0900

    refactor: 8.6 문장 슬라이드하기

--- src/main/java/com/example/refactoring2/ch08/ex6/BillingService.java
+++ src/main/java/com/example/refactoring2/ch08/ex6/BillingService.java
@@ -12,19 +12,23 @@ public class BillingService {
   }
 
   public double processOrder(Long orderId) {
+    // 가격 정책과 주문 정보를 가져온다
     PricingPlan pricingPlan = pricingPlanRepository.findPricingPlan();
     Order order = orderRepository.findById(orderId);
 
-    double charge = 0.0;
+    // 기본 청구 금액을 계산한다
     int baseCharge = pricingPlan.getBase();
     int chargePerUnit = pricingPlan.getUnitCharge();
     int units = order.getUnits();
-    double discount = 0;
-    charge = baseCharge + units * chargePerUnit;
+    double charge = baseCharge + units * chargePerUnit;
+
+    // 할인 금액을 계산한다
     int discountableUnits = Math.max(units - pricingPlan.getDiscountThreshold(), 0);
-    discount = discountableUnits * pricingPlan.getDiscountFactor();
-    if (order.isRepeatOrder()) discount += 20;
-    charge = charge - discount;
-    return charge;
+    double discount = discountableUnits * pricingPlan.getDiscountFactor();
+    if (order.isRepeatOrder()) {
+      discount += 20;
+    }
+
+    return charge - discount;
   }
 }
