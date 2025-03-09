7269af8f6a4e2c5bcacad2b8c1358027bae80866
이진우 <leejinwoo1126@gmail.com>
2025-03-09 17:14:14 +0900

    refactor: 8.8 Stream 파이프라인으로 리팩터링

--- src/main/java/com/example/refactoring2/ch08/ex8/IndiaSearch.java
+++ src/main/java/com/example/refactoring2/ch08/ex8/IndiaSearch.java
@@ -1,29 +1,18 @@
 package com.example.refactoring2.ch08.ex8;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class IndiaSearch {
   private IndiaSearch() {}
 
   public static List<SearchResult> acquireData(String input) {
-    String[] lines = input.split("\n");
-    boolean firstLine = true;
-    List<SearchResult> result = new ArrayList<>();
-    for (String line : lines) {
-      if (firstLine) {
-        firstLine = false;
-        continue;
-      }
-
-      if (line.trim().equals("")) continue;
-
-      String[] record = line.split(",");
-      if (record[1].trim().equals("India")) {
-        result.add(new SearchResult(record[0].trim(), record[2].trim()));
-      }
-    }
-
-    return result;
+    return Arrays.stream(input.split("\n"))
+        .skip(1)
+        .filter(line -> !line.trim().isEmpty())
+        .map(line -> line.split(","))
+        .filter(record -> record[1].trim().equals("India"))
+        .map(record -> new SearchResult(record[0].trim(), record[2].trim()))
+        .toList();
   }
 }
