b67b6ea8798556f50a2e41388a080f5ae297a188
이진우 <leejinwoo1126@gmail.com>
2025-03-23 17:30:27 +0900

    test: 10.4 조건부 로직을 다형성으로 바꾸기 예시1

---
+++ src/test/java/com/example/refactoring2/ch10/ex4/BirdClassifierTest.java
@@ -0,0 +1,50 @@
+package com.example.refactoring2.ch10.ex4;
+
+import static org.assertj.core.api.Assertions.*;
+
+import java.util.List;
+import java.util.Map;
+import org.junit.jupiter.api.Test;
+
+class BirdClassifierTest {
+
+  @Test
+  void plumagesTest() {
+    List<Bird> birds =
+        List.of(
+            create("이름1", "유럽 제비"),
+            create("이름2", "아프리카 제비"),
+            create("이름3", "노르웨이 파랑 앵무"),
+            create("이름4", "unknown"));
+
+    Map<String, String> actual = BirdClassifier.plumages(birds);
+
+    assertThat(actual)
+        .containsEntry("이름1", "보통이다")
+        .containsEntry("이름2", "보통이다")
+        .containsEntry("이름3", "예쁘다")
+        .containsEntry("이름4", "알 수 없다");
+  }
+
+  @Test
+  void speedsTest() {
+    List<Bird> birds =
+        List.of(
+            create("이름1", "유럽 제비"),
+            create("이름2", "아프리카 제비"),
+            create("이름3", "노르웨이 파랑 앵무"),
+            create("이름4", "unknown"));
+
+    Map<String, Integer> actual = BirdClassifier.speeds(birds);
+
+    assertThat(actual)
+        .containsEntry("이름1", 35)
+        .containsEntry("이름2", 40)
+        .containsEntry("이름3", 10)
+        .containsEntry("이름4", 0);
+  }
+
+  private static Bird create(String name, String type) {
+    return new Bird(name, type, 0, 0, false);
+  }
+}
