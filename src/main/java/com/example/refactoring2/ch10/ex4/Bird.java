0d878b75b0877489dbc0a6a30a8e008a5a8a8e30
이진우 <leejinwoo1126@gmail.com>
2025-03-23 17:34:52 +0900

    refactor: 10.4 Bird 클래스 변경, 함수 이동

--- src/main/java/com/example/refactoring2/ch10/ex4/Bird.java
+++ src/main/java/com/example/refactoring2/ch10/ex4/Bird.java
@@ -1,3 +1,59 @@
 package com.example.refactoring2.ch10.ex4;
 
-public record Bird(String name, String type, int numberOfCoconuts, int voltage, boolean isNailed) {}
+public class Bird {
+  private final String name;
+  private final String type;
+  private final int numberOfCoconuts;
+  private final int voltage;
+  private final boolean isNailed;
+
+  public Bird(Bird bird) {
+    this(bird.name, bird.type, bird.numberOfCoconuts, bird.voltage, bird.isNailed);
+  }
+
+  public Bird(String name, String type, int numberOfCoconuts, int voltage, boolean isNailed) {
+    this.name = name;
+    this.type = type;
+    this.numberOfCoconuts = numberOfCoconuts;
+    this.voltage = voltage;
+    this.isNailed = isNailed;
+  }
+
+  public String plumage() {
+    return switch (type) {
+      case "유럽 제비" -> "보통이다";
+      case "아프리카 제비" -> (numberOfCoconuts > 2) ? "지쳤다" : "보통이다";
+      case "노르웨이 파랑 앵무" -> (voltage > 100) ? "그을렸다" : "예쁘다";
+      default -> "알 수 없다";
+    };
+  }
+
+  public Integer airSpeedVelocity() {
+    return switch (type) {
+      case "유럽 제비" -> 35;
+      case "아프리카 제비" -> 40 - 2 * numberOfCoconuts;
+      case "노르웨이 파랑 앵무" -> isNailed ? 0 : 10 + voltage / 10;
+      default -> 0;
+    };
+  }
+
+  public String name() {
+    return name;
+  }
+
+  public String type() {
+    return type;
+  }
+
+  public int numberOfCoconuts() {
+    return numberOfCoconuts;
+  }
+
+  public int voltage() {
+    return voltage;
+  }
+
+  public boolean isNailed() {
+    return isNailed;
+  }
+}
--- src/main/java/com/example/refactoring2/ch10/ex4/BirdClassifier.java
+++ src/main/java/com/example/refactoring2/ch10/ex4/BirdClassifier.java
@@ -17,20 +17,10 @@ public class BirdClassifier {
   }
 
   private static String plumage(Bird bird) {
-    return switch (bird.type()) {
-      case "유럽 제비" -> "보통이다";
-      case "아프리카 제비" -> (bird.numberOfCoconuts() > 2) ? "지쳤다" : "보통이다";
-      case "노르웨이 파랑 앵무" -> (bird.voltage() > 100) ? "그을렸다" : "예쁘다";
-      default -> "알 수 없다";
-    };
+    return new Bird(bird).plumage();
   }
 
   private static Integer airSpeedVelocity(Bird bird) {
-    return switch (bird.type()) {
-      case "유럽 제비" -> 35;
-      case "아프리카 제비" -> 40 - 2 * bird.numberOfCoconuts();
-      case "노르웨이 파랑 앵무" -> bird.isNailed() ? 0 : 10 + bird.voltage() / 10;
-      default -> 0;
-    };
+    return new Bird(bird).airSpeedVelocity();
   }
 }
