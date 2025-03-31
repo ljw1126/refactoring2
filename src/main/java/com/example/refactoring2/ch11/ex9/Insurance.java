bc1ff680cb33356a92616b4ea12966ef5a5ede08
이진우 <leejinwoo1126@gmail.com>
2025-03-31 16:48:22 +0900

    refactor: 11.9 함수를 명령으로 바꾸기 예시

--- src/main/java/com/example/refactoring2/ch11/ex9/Insurance.java
+++ src/main/java/com/example/refactoring2/ch11/ex9/Insurance.java
@@ -3,24 +3,6 @@ package com.example.refactoring2.ch11.ex9;
 public class Insurance {
 
   public int score(Candidate candidate, MedicalExam medicalExam, ScoringGuide scoringGuide) {
-    int result = 0;
-    int healthLevel = 0;
-    boolean highMedicalRiskFlag = false;
-
-    if (medicalExam.isSmoker()) {
-      healthLevel += 10;
-      highMedicalRiskFlag = true;
-    }
-
-    String certificationGrade = "regular";
-    if (scoringGuide.startWithLowCertification(candidate.originState())) {
-      certificationGrade = "low";
-      result -= 5;
-    }
-
-    // do something ..
-
-    result -= Math.max(healthLevel - 5, 0);
-    return result;
+    return new Score(candidate, medicalExam, scoringGuide).execute();
   }
 }
