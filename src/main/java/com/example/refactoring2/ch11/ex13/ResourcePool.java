bda89099ba19be91f02c9944734d7c942ad4ceee
이진우 <leejinwoo1126@gmail.com>
2025-03-31 17:48:53 +0900

    refactor: 11.13 예외를 사전확인으로 바꾸기 예시

--- src/main/java/com/example/refactoring2/ch11/ex13/ResourcePool.java
+++ src/main/java/com/example/refactoring2/ch11/ex13/ResourcePool.java
@@ -16,16 +16,8 @@ public class ResourcePool {
   }
 
   public Resource get() {
-    Resource result;
-
-    try {
-      result = available.pop();
-      allocated.add(result);
-    } catch (NoSuchElementException e) {
-      result = Resource.create();
-      allocated.add(result);
-    }
-
+    Resource result = available.isEmpty() ? Resource.create() : available.pop();
+    allocated.add(result);
     return result;
   }
 }
