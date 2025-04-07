9b07c7a25958b8a9f24cc4faa7ddb754e92b8678
이진우 <leejinwoo1126@gmail.com>
2025-04-07 19:52:19 +0900

    fix: 12.10 패키지명 수정

---
+++ src/main/java/com/example/refactoring2/ch12/ex10/part1/Booking.java
@@ -0,0 +1,53 @@
+package com.example.refactoring2.ch12.ex10.part1;
+
+import java.time.LocalDate;
+
+public class Booking {
+  protected final Show show;
+  protected final LocalDate date;
+  protected final boolean peakDay;
+
+  protected PremiumDelegate premiumDelegate;
+
+  protected Booking(Show show, LocalDate date, boolean peakDay) {
+    this.show = show;
+    this.date = date;
+    this.peakDay = peakDay;
+  }
+
+  public static Booking createBooking(Show show, LocalDate date, boolean peakDay) {
+    return new Booking(show, date, peakDay);
+  }
+
+  public static Booking createPremiumBooking(
+      Show show, LocalDate date, boolean peakDay, Extras extras) {
+    Booking result = new Booking(show, date, peakDay);
+    result.bePremium(extras);
+    return result;
+  }
+
+  protected void bePremium(Extras extras) {
+    this.premiumDelegate = new PremiumDelegate(this, extras);
+  }
+
+  public boolean hasTalkback() {
+    return (this.premiumDelegate != null)
+        ? this.premiumDelegate.hasTalkback()
+        : this.show.isTalkback() && !peakDay;
+  }
+
+  public long basePrice() {
+    long result = this.show.price();
+    if (peakDay) result += Math.round(result * 0.15);
+
+    return (this.premiumDelegate != null) ? this.premiumDelegate.extendBasePrice(result) : result;
+  }
+
+  public boolean hasDinner() {
+    return (this.premiumDelegate != null) && this.premiumDelegate.hasDinner();
+  }
+
+  public Show getShow() {
+    return show;
+  }
+}
--- src/main/java/com/example/refactoring2/ch12/ex3/Employee.java
+++ src/main/java/com/example/refactoring2/ch12/ex3/Employee.java
@@ -3,25 +3,25 @@ package com.example.refactoring2.ch12.ex3;
 import java.util.Objects;
 
 public class Employee extends Party {
-    private final Long id;
-    private final int monthlyCost;
+  private final Long id;
+  private final int monthlyCost;
 
-    public Employee(Long id, String name, int monthlyCost) {
-        super(name);
-        this.id = id;
-        this.monthlyCost = monthlyCost;
-    }
+  public Employee(Long id, String name, int monthlyCost) {
+    super(name);
+    this.id = id;
+    this.monthlyCost = monthlyCost;
+  }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Employee employee = (Employee) o;
-        return monthlyCost == employee.monthlyCost && Objects.equals(id, employee.id);
-    }
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+    Employee employee = (Employee) o;
+    return monthlyCost == employee.monthlyCost && Objects.equals(id, employee.id);
+  }
 
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, monthlyCost);
-    }
+  @Override
+  public int hashCode() {
+    return Objects.hash(id, monthlyCost);
+  }
 }
